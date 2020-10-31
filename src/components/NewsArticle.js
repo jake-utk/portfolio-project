83d8d55ec2bc2acdd22a3d39bc35df7382a3a724
jake-utk <jakeryanaddis@gmail.com>
2020-10-31 12:14:42 -0500

    added story text when available

--- src/components/NewsArticle.js
+++ src/components/NewsArticle.js
@@ -2,15 +2,23 @@ import React from "react";
 
 const NewsArticle = ({ article }) => {
 	console.log("should be single index of res.hits", article);
-	return (
-		<div className='newsArticle'>
-			<h3 id='title'>{article.title}</h3>
-			<h4 id='author'>Author: {article.author}</h4>
-			<h4 id='url'>
-				<a href={article.url}>Link to Article </a>
-			</h4>
-		</div>
-	);
+	console.log('title here, trying to hide if null: ', article.title);
+	console.log('trying to access story_text: ', article.story_text);
+
+	if (article.title === null) {
+		return null;
+	} else {
+		return (
+			<div className='newsArticle'>
+				<h3 id='title'>TITLE: {article.title}</h3>
+				<h4 id='author'>Author: {article.author}</h4>
+				<h4 id='url'>
+					<a href={article.url}>Link to Article </a>
+				</h4>
+				<p id='story_text'>{article.story_text}</p>
+			</div>
+		);
+	}
 };
 
 export default NewsArticle;
